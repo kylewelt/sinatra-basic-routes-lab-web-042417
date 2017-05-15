10883c42a05e5b56b63ab2957abdb95a05030759
kylewelt <kylewelt@umich.edu>
2017-05-15 12:57:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kyle"
+  end
+
+  get '/hometown' do
+    "My hometown is St. Johns, MI"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Noisestorm - Sentinel"
+  end
+
 end
